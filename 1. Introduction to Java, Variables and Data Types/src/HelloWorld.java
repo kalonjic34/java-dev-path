c40f785cb10cb269498198351dad76294c92e761
Christian <kalonjic34@gmail.com>
2024-12-06 15:11:10 +0200

    Hello World

---
+++ 1. Introduction to Java, Variables and Data Types/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Hello, Chris");
+        System.out.println("Hello, Programmer");
+    }
+}
