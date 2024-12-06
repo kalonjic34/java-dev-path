ad28d7b8801093f82ee7c14cf9e2f0023b18d3a0
Christian <kalonjic34@gmail.com>
2024-12-06 15:26:12 +0200

    print vs println

---
+++ 1. Introduction to Java, Variables and Data Types/src/PrintVsPrintln.java
@@ -0,0 +1,13 @@
+public class PrintVsPrintln {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.print("Hello, Chris");
+
+        System.out.println();
+        System.out.println();
+
+        // Escape Sequence with \n
+        System.out.println("Hello, \nWorld");
+        System.out.println("Welcome");
+    }
+}
