8cae429adfa105f24d17e45d4cc4957f6ae1cf0f
Christian <kalonjic34@gmail.com>
2024-12-06 15:57:19 +0200

    Numbers and math

---
+++ 1. Introduction to Java, Variables and Data Types/src/NumbersAndMath.java
@@ -0,0 +1,15 @@
+public class NumbersAndMath {
+    public static void main(String[] args) {
+        System.out.println("Addition");
+        System.out.println(4+2);
+        
+        System.out.println("Subtraction");
+        System.out.println(5-2);
+
+        System.out.println("Multiplication");
+        System.out.println(5*2);
+
+        System.out.println("Division");
+        System.out.println(10/2);
+    }
+}
