573af0d6aa29fedee51f740f54974495544d95fe
Christian <kalonjic34@gmail.com>
2024-12-07 15:49:52 +0200

    Relational Operators

---
+++ 2. Handling User Input and Simple Calculations/src/RelationalOperators.java
@@ -0,0 +1,31 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        // Relational Operators
+        int a = 6;
+        int b = 5;
+        
+        // Equal Operator
+        boolean result = (a == b);
+        System.out.println(result);
+
+        // not Equal Operator
+        result = (a != b);
+        System.out.println(result);
+
+        // greater than Operator >
+        result = (a > b); 
+        System.out.println(result);
+
+        // less than Operator <
+        result = (a < b);
+        System.out.println(result);
+
+        // less than or equal Operator <=
+        result = (a <= b);
+        System.out.println("less than or equal "+result);
+
+        // greater than or equal Operator <=
+        result = (a <= b);
+        System.out.println("greater than or equal "+result);
+    }
+}
