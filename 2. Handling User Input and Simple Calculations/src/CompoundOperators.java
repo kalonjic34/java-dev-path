4115642f6e938eca4d23e3a39a37f2d0f6d99eea
Christian <kalonjic34@gmail.com>
2024-12-07 15:56:34 +0200

    Compound Operators

---
+++ 2. Handling User Input and Simple Calculations/src/CompoundOperators.java
@@ -0,0 +1,26 @@
+public class CompoundOperators {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+
+        // Addition assignment
+        a+= 3; // a = a + 3;
+        System.out.println("after addition assignment "+a);
+
+        // Substraction assignment
+        a-= 3; // a = a + 3;
+        System.out.println("after substraction assignment "+a);
+
+        // Multiplication assignment
+        a*= 3; // a = a + 3;
+        System.out.println("after multiplication assignment "+a);
+
+        // Division assignment
+        a/= 3; // a = a + 3;
+        System.out.println("after division assignment "+a);
+
+        // Modulo assignment
+        a%= 3; // a = a + 3;
+        System.out.println("after modulo assignment "+a);
+    }
+}
