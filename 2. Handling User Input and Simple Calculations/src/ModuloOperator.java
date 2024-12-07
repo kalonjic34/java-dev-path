646ad7d62100d8e7923dca94c23063db04d5ac9c
Christian <kalonjic34@gmail.com>
2024-12-07 15:29:55 +0200

    Modulo Operator

---
+++ 2. Handling User Input and Simple Calculations/src/ModuloOperator.java
@@ -0,0 +1,28 @@
+public class ModuloOperator {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 8;
+        int result;
+
+        // Addition
+        result = num1 + num2;
+        System.out.print("Addition result: "+result);
+
+        // Substraction
+        result = num1 - num2;
+        System.out.print("\nSubstraction result: "+result);
+
+        // Multiplication
+        result = num1 * num2;
+        System.out.print("\nMultiplication result: "+result);
+
+        // Division
+        result = num1 / num2;
+        System.out.print("\nDivision result: "+result);
+
+        // Modulo
+        result = num2 % num1;
+        System.out.println("\nModulo result: "+result);
+
+    }
+}
