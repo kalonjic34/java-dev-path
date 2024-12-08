85c5aa187bf1d67929c9429ddca6a2452fdedcb7
Christian <kalonjic34@gmail.com>
2024-12-08 14:19:38 +0200

    Strings In Java

---
+++ 4. More Practice - Java Fundamentals/src/Strings.java
@@ -0,0 +1,23 @@
+public class Strings {
+    public static void main(String[] args) {
+        // var message = "Hello World";
+        // var extra = "I love to code!";
+
+        String message = "Hello World.";
+        String extra = "I love to code!";
+
+        System.out.println(message + " "+ extra);
+
+        // Call methods on String: length(), toUpperCase(), toLowerCase()
+        
+        System.out.println("Length of "+message+" is "+ message.length());
+        System.out.println("Upper case version of "+message+" is "+ message.toUpperCase());
+        System.out.println("Lower case version of "+message+" is "+ message.toLowerCase());
+
+        String combo = message +" "+extra;
+        System.out.println("Combo: "+combo);
+
+        String upperVersion = combo.toUpperCase();
+        System.out.println("Upper version: "+upperVersion);
+    }
+}
