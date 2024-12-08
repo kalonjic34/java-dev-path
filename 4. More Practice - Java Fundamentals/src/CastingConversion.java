b9f924e0e7775a05540fee98ac25b57c657fc206
Christian <kalonjic34@gmail.com>
2024-12-08 15:23:12 +0200

    Casting and Conversion

---
+++ 4. More Practice - Java Fundamentals/src/CastingConversion.java
@@ -0,0 +1,30 @@
+public class CastingConversion {
+    public static void main(String[] args) {
+        
+        // convert double to int
+        double theDoubleGradeAvg = 89.70;
+        int theIntGradeAvg = (int) theDoubleGradeAvg;
+
+        System.out.println("theDoubleGradeAvg= "+theDoubleGradeAvg);
+        System.out.println("theIntGradeAvg= "+theIntGradeAvg);
+
+        // convert float to byte
+        float theFloatDistance = 123.60f;
+        float theByteDistance = (byte) theFloatDistance;
+
+        System.out.println("theFloatDistance= "+theFloatDistance);
+        System.out.println("theByteDistance= "+theByteDistance);
+    
+        // convert int to char
+        int theCharacterCode = 65;
+        char theChar = (char) theCharacterCode;
+        
+        System.out.println("theCharacterCode= "+theCharacterCode);
+        System.out.println("theChar= "+theChar);
+
+        // convert String to int
+        int count = Integer.parseInt("49");
+        System.out.println("count="+count);
+
+    }
+}
