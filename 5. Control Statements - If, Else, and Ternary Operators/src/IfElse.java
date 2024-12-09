7fa1b576114f5956f339e09cba7d58eb19c3cf48
Christian <kalonjic34@gmail.com>
2024-12-09 12:38:12 +0200

    If Else Control Statements

---
+++ 5. Control Statements - If, Else, and Ternary Operators/src/IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main(String[] args) {
+        // int number = 10;
+        int number = 5;
+
+        if (number % 2==0) {
+            System.out.println("This number is even");
+        }else{
+            System.out.println("This number is odd");
+        }
+    }
+}
