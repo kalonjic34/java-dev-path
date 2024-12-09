28c1d35ab9ad8db3d95c89de8562a0630beff4c1
Christian <kalonjic34@gmail.com>
2024-12-09 12:43:15 +0200

    Nested If Else Control Statements

---
+++ 5. Control Statements - If, Else, and Ternary Operators/src/NestedIfElse.java
@@ -0,0 +1,14 @@
+public class NestedIfElse {
+    public static void main(String[] args) {
+        // nested  if else control statement
+        
+        int number = -5;
+        if (number > 0) {
+            System.out.println("Number is positive");
+        }else if(number < 0){
+            System.out.println("Number is negative");
+        }else{
+            System.out.println("Number is 0");
+        }
+    }
+}
