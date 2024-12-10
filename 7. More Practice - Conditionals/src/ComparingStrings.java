fda18e04d4a3ec3c850ccceed1878494ab317d4c
Christian <kalonjic34@gmail.com>
2024-12-10 13:21:45 +0200

    Conditionals Comparing Strings

---
+++ 7. More Practice - Conditionals/src/ComparingStrings.java
@@ -0,0 +1,34 @@
+public class ComparingStrings {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        boolean result1 = s1 == s2;
+
+        System.out.println("s1: "+s1);
+        System.out.println("s2: "+s2);
+        System.out.println("s1 == s2: "+result1);
+        System.out.println();
+
+        // == operater compares to the memory address 
+        // Not the contents of the String
+
+        String s3 = new String("Hello");
+        boolean result3 = s1 == s3;
+
+        System.out.println("s1: "+s1);
+        System.out.println("s3: "+s3);
+        System.out.println("s1 == s3: "+result3);
+        System.out.println();
+
+        System.out.println("s3: "+s3);
+        result3 = s1.equals(s3);
+        // Compares contents of the String
+        System.out.println("s1. equals(s3): "+result3);
+        System.out.println();
+
+        String s4 = new String("hello");
+        System.out.println("s4: "+s4);
+        boolean result4 = s4.equalsIgnoreCase(s3);
+        System.out.println("s4.equalsIgnoreCase(s3): "+result4);
+    }
+}
