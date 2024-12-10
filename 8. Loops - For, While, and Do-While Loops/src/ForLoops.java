7caa43c0f6c9f122aaab583bf5e67b531218b0cb
Christian <kalonjic34@gmail.com>
2024-12-10 19:21:48 +0200

    For Loops

---
+++ 8. Loops - For, While, and Do-While Loops/src/ForLoops.java
@@ -0,0 +1,13 @@
+public class ForLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("This is after the loop\n");
+        
+        
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+} 
