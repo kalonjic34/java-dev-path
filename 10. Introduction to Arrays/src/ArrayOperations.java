65804e9f47d08db2a470fc65189763bfcb1e9526
Christian <kalonjic34@gmail.com>
2024-12-11 15:52:46 +0200

    Declaring, Initializing, Accessing and Modifying arrays

---
+++ 10. Introduction to Arrays/src/ArrayOperations.java
@@ -0,0 +1,12 @@
+public class ArrayOperations {
+    public static void main(String[] args) {
+        int[] numbers; // Declaring the array but not taking space
+        numbers = new int[5]; // initializing
+
+        int[] numbers2 ={1,2,3,4,5};
+
+        numbers2[0] =420;
+        int firstNum = numbers2[0];
+        System.out.println(firstNum);
+    }
+}
