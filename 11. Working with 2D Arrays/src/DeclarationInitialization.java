74f5ad7986bc6a9814c8235724837da5b3b70e74
Christian <kalonjic34@gmail.com>
2024-12-12 14:09:01 +0200

    Iterating through columns with an enhance for loop

--- 11. Working with 2D Arrays/src/DeclarationInitialization.java
+++ 11. Working with 2D Arrays/src/DeclarationInitialization.java
@@ -9,10 +9,10 @@ public class DeclarationInitialization {
 
         System.out.println("My2d Array length: "+my2DArray.length);
 
-        for (int i = 0; i < my2DArray.length; i++) {
+        for (int[] rows : my2DArray) {
             //  iterates through columns
-            for (int j = 0; j < my2DArray[i].length; j++) {
-                System.out.print(my2DArray[i][j]+" ");
+            for (int num : rows) {
+                System.out.print(num+" ");
             }
             System.out.println();
         }
