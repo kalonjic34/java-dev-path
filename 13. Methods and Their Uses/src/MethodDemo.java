ac66561e595bf4f4a8e94ccb07f7a8a971e2c573
Christian <kalonjic34@gmail.com>
2024-12-15 15:20:46 +0200

    Getting Started with Methods

---
+++ 13. Methods and Their Uses/src/MethodDemo.java
@@ -0,0 +1,13 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+        // call the medthod
+        displayGreetings();
+    }
+
+    //define the method
+    static void displayGreetings(){
+        System.out.println("Hello world!");
+        System.out.println("Welcome, welcome.");
+        System.out.println("Please make yourself at home.");
+    }
+}
