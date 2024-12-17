8db167ae284ffe08fe89dbeefe39a96f028f5296
Christian <kalonjic34@gmail.com>
2024-12-17 19:31:07 +0200

    BufferedReader

--- 16. Exception Handling & File IO/src/Main.java
+++ 16. Exception Handling & File IO/src/Main.java
@@ -1,27 +1,24 @@
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        FileReader fileReader = null;
+        BufferedReader reader = null;
 
-        try{
-            // connect to a new file
-            fileReader = new FileReader("story.txt");
-
-            // -1 means the file ends
-            int character = 0;
-            while ((character = fileReader.read()) != - 1 ){
-                System.out.println((char)character);
+        try {
+            reader = new BufferedReader(new FileReader("story.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
             }
-        }
-        catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println(e);
         }
         finally{
-            if (fileReader != null) {
-                fileReader.close();
+            if (reader != null) {
+                reader.close();
             }
         }
     }
