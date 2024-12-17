119c1b26355330561a37de7232e596ae67de9add
Christian <kalonjic34@gmail.com>
2024-12-17 19:34:14 +0200

    Try with Resources

--- 16. Exception Handling & File IO/src/Main.java
+++ 16. Exception Handling & File IO/src/Main.java
@@ -5,21 +5,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException{
-        BufferedReader reader = null;
 
-        try {
-            reader = new BufferedReader(new FileReader("story.txt"));
+        try (BufferedReader reader = new BufferedReader(new FileReader("story.txt"))){
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
         } catch (FileNotFoundException e) {
             System.out.println(e);
-        }
-        finally{
-            if (reader != null) {
-                reader.close();
-            }
         }
     }
 }
