5f48a302345bb62aa9af6302cba62a30bbc5194a
Christian <kalonjic34@gmail.com>
2024-12-17 19:11:40 +0200

    File Input and Throw

--- 16. Exception Handling & File IO/src/Main.java
+++ 16. Exception Handling & File IO/src/Main.java
@@ -1,25 +1,16 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
     public static void main(String[] args) {
-        // String numberString = "32";
-        // parseString("love2code");
-        // parseString(null);
-        parseString("32");
+        try{
+            readFile();
+        } catch(FileNotFoundException e){
+            System.out.println(e);
+        }
+    }  
+    public static void readFile() throws FileNotFoundException{
+        FileReader fileReader = new FileReader("file.txt");
 
     }
-
-        public static void parseString(String numberString){
-            try {
-                int numberInt = Integer.parseInt(numberString);
-                System.out.println(numberInt);  
-            } 
-            catch (NumberFormatException | NullPointerException e) {
-                System.out.println("String needs to be a valid int.");
-            } 
-            catch(Exception e){
-                System.out.println(e);
-            }
-            finally{
-                System.out.println("In the finally block.");
-            }
-    } 
 }
