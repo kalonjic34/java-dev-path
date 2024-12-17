c04621dea507c66c09988e1e5c5147649cc9f775
Christian <kalonjic34@gmail.com>
2024-12-17 19:56:07 +0200

    BufferedWriter

--- 16. Exception Handling & File IO/src/Main.java
+++ 16. Exception Handling & File IO/src/Main.java
@@ -1,10 +1,20 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException{
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter("file.txt"))) {
+            writer.write("Hello World!");
+            writer.newLine();
+            writer.write("This is a simple text file");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
 
         try (BufferedReader reader = new BufferedReader(new FileReader("story.txt"))){
             String line;
